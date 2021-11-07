7368478a3f6aa11dc5e668db53cab24c3dea0dda
ErickAguilar009 <erickbvb009@gmail.com>
2021-11-07 17:59:37 -0600

    Vistas de Favorites

--- ProyectoDpsClient/src/components/Product/Favorite.js
+++ ProyectoDpsClient/src/components/Product/Favorite.js
@@ -1,25 +1,65 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button } from "react-native-paper";
+import { size } from "lodash";
+import useAuth from "../../hooks/useAuth";
+import {
+    addFavoriteApi,
+    isFavoriteApi,
+    deleteFavoriteApi,
+} from "../../api/favorite";
 
 export default function Favorite(props) {
     const { product } = props;
+    const { auth } = useAuth();
+    const [isFavorite, setIsFavorite] = useState(undefined);
+    const [loading, setLoading] = useState(false);
+    const [reloadFavorite, setReloadFavorite] = useState(false);
 
-    const addFavorite = () => {
-        console.log("Producto agregado a favoritos");
-        console.log(product.title);
-    }
+    useEffect(() => {
+        (async () => {
+            const response = await isFavoriteApi(auth, product._id);
+            if (size(response) === 0) setIsFavorite(false);
+            else setIsFavorite(true);
+
+            setReloadFavorite(false);
+            setLoading(false);
+        })();
+    }, [product, reloadFavorite]);
+
+    const addFavorite = async () => {
+        if (!loading) {
+            setLoading(true);
+            await addFavoriteApi(auth, product._id);
+            setReloadFavorite(true);
+        }
+    };
+
+    const deleteFavorite = async () => {
+        if (!loading) {
+            setLoading(true);
+            await deleteFavoriteApi(auth, product._id);
+            setReloadFavorite(true);
+        }
+    };
+
+    if (isFavorite === undefined) return null;
 
     return (
         <View style={{ zIndex: -1 }}>
             <Button
-                mode='contained'
-                contentStyle={styles.btnAddFavoritesContent}
+                mode="contained"
+                contentStyle={
+                    isFavorite
+                        ? styles.btnDeleteFavoritesContent
+                        : styles.btnAddFavoritesContent
+                }
                 labelStyle={styles.btnLabel}
                 style={styles.btn}
-                onPress={addFavorite}
+                onPress={isFavorite ? deleteFavorite : addFavorite}
+                loading={loading}
             >
-                Agregar a favoritos
+                {isFavorite ? "Eliminar de favoritos" : "Añadir a favoritos"}
             </Button>
         </View>
     )
@@ -27,12 +67,17 @@ export default function Favorite(props) {
 
 const styles = StyleSheet.create({
     btnLabel: {
-        fontSize: 18
-    },
-    btnAddFavoritesContent: {
-        backgroundColor: "#057b00"
+        fontSize: 18,
     },
     btn: {
-        marginTop: 20
-    }
-})
+        marginTop: 20,
+    },
+    btnAddFavoritesContent: {
+        backgroundColor: "#057b00",
+        paddingVertical: 5,
+    },
+    btnDeleteFavoritesContent: {
+        backgroundColor: "#c40000",
+        paddingVertical: 5,
+    },
+});
--- ProyectoDpsClient/src/screens/Favorites.js
+++ ProyectoDpsClient/src/screens/Favorites.js
@@ -1,18 +1,63 @@
-import React from 'react'
+import React, { useState, useCallback } from "react";
 import { View, Text, StyleSheet } from 'react-native';
+import { useFocusEffect } from "@react-navigation/native";
+import { size } from "lodash";
+import StatusBarCustom from '../components/StatusBarCustom';
+import Search from "../components/Search";
+import useAuth from "../hooks/useAuth";
+import ScreenLoading from "../components/ScreenLoading";
+import FavoriteList from "../components/Favorites/FavoriteList";
+import { getFavoriteApi } from "../api/favorite";
+import colors from "../styles/colors";
 
 export default function Favorites() {
+    const [products, setProducts] = useState(null);
+    const [reloadFavorite, setReloadFavorite] = useState(false);
+    const { auth } = useAuth();
+
+    useFocusEffect(
+        useCallback(() => {
+            setProducts(null);
+
+            (async () => {
+                const response = await getFavoriteApi(auth);
+                setProducts(response);
+            })();
+
+            setReloadFavorite(false);
+        }, [reloadFavorite])
+    );
+
     return (
-        <View style={styles.container}>
-            <Text>Estamos en Favoritos</Text>
-        </View>
-    )
+        <>
+            <StatusBarCustom backgroundColor={colors.bgDark} barStyle="light-content" />
+            <Search />
+            {!products ? (
+                <ScreenLoading text="Cargando lista" />
+            ) : size(products) === 0 ? (
+                <View style={styles.container}>
+                    <Text style={styles.title}>Lista de favoritos</Text>
+                    <Text>No tienes productos en tu lista</Text>
+                </View>
+            ) : (
+                <FavoriteList
+                    products={products}
+                    auth={auth}
+                    setReloadFavorite={setReloadFavorite}
+                />
+            )}
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 19,
+        marginBottom: 5,
+    },
 });
